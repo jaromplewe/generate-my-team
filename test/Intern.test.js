ba415defd553f19e67689dda3487bb4f078934f8
Jarom Plewe <jplewe23@gmail.com>
2020-10-02 18:42:13 -0600

    intern.test passes

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -2,18 +2,18 @@ const Intern = require("../lib/Intern");
 
 test("Can set school via constructor", () => {
   const testValue = "UCLA";
-  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  const e = new Intern("Foo", "Bar", 1, "test@test.com", testValue);
   expect(e.school).toBe(testValue);
 });
 
 test("getRole() should return \"Intern\"", () => {
   const testValue = "Intern";
-  const e = new Intern("Foo", 1, "test@test.com", "UCLA");
+  const e = new Intern("Foo", testValue, 1, "test@test.com", "UCLA");
   expect(e.getRole()).toBe(testValue);
 });
 
 test("Can get school via getSchool()", () => {
   const testValue = "UCLA";
-  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  const e = new Intern("Foo", "Bar", 1, "test@test.com", testValue);
   expect(e.getSchool()).toBe(testValue);
 });
